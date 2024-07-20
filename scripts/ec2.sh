ba648d42ab88007ce82721aff8c95095fc1e714e
kota-yata <kota@yatagai.com>
2024-07-20 14:54:28 -0700

    add certs script for ec2

---
+++ scripts/ec2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Create certs dir if it does not exists
+mkdir -p ./certs
+
+# Generate publi - private key 
+openssl req -newkey rsa:2048 -nodes -keyout ./certs/certificate.key -x509 -out ./certs/certificate.pem -subj '/CN=54.187.49.238' -addext "subjectAltName=IP:54.187.49.238"
+
+openssl x509 -outform der -in ./certs/certificate.pem -out ./certificate.crt
